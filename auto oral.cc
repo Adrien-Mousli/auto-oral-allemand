4bb887b039eceee4c04368aa105dac414ba1ca8f
Adrien-Mousli <49747260+Adrien-Mousli@users.noreply.github.com>
2019-09-29 19:28:19 +0200

    Add files via upload

---
+++ auto oral.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+string quest1={};
+string quest2={};
+string quest3={};
+string quest4={};
+string quest5={};
+string quest6={};
+string quest7={};
+string quest8={};
+string quest9={};
+string quest10={};
+string quest11={};
+string quest12={};
+string quest13={};
+string quest14={};
+string quest15={};
+string quest16={};
+string quest17={};
+string quest18={};
+string quest19={};
+string quest20={};
+string quest21={};
+string quest22={};
+string quest23={};
+string quest24={};
+string quest25={};
+
+                                            // <----------- laisse un vide au cas besoin de nouv variable
+
+cout << "il faudrat écrire les réponses en allemand" << endl;
+// mettre plus tard une vérif réponce uttilisateur
+cout << "comment t'appelle tu ?" << endl;
+cin >> quest1;
+
+cout << "quelle âge a tu ?" << endl;
+cin >> quest2;
+
+cout << "as-tu des frère(s)/soeur(s) (ne pas répondre par oui ou non)" << endl;
+cin >> quest3;
+
+cout << "ou habite tu ?" << endl;
+cin >> quest4;
+
+
+cout << "hallo meine name ist " << quest1 << " ich habe " << quest2 << " Jahre alt " << quest3 << " Ich wohne in " << quest4 << endl; // rappel: mettre des espaces a chaque bout de texte
+
+return 0;
+}
